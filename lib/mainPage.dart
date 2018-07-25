500dedf53f3085e1504c6383f3792e93db4597a8
MasterUNG <phrombutr@gmail.com>
2018-07-25 09:18:54 +0700

    Project Success

--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -13,43 +13,87 @@ class _StatefulWidget extends StatefulWidget {
     // TODO: implement createState
     return new __StatefulWidgetState();
   }
-
 }
 
 class __StatefulWidgetState extends State<_StatefulWidget> {
   var title = '';
+
   Drawer drawer(context) {
     return new Drawer(
       child: new ListView(
+        padding: EdgeInsets.zero,
         children: <Widget>[
           new DrawerHeader(
-              child: new Container(
-                child: new Column(
-                  children: <Widget>[
-                    new Image.asset(
-                      'images/masterung_round.png',
-                      width: 80.0,
-                      height: 80.0,
+            child: new Container(
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  new Image.asset(
+                    'images/masterung_round.png',
+                    width: 80.0,
+                    height: 80.0,
+                  ),
+                  new Text(
+                    "Master UNG",
+                    style: new TextStyle(
+                        color: Colors. white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25.0),
+                  ),
+                  new Text(
+                    "Developers Manager",
+                    style: new TextStyle(
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white,
                     ),
-                    new Text(
-                      "Master UNG",
-                      style: new TextStyle(
-                          color: Colors.blue,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 25.0
-                        ),),
-                    new Text(
-                      "Developers Manager",
-                      style: new TextStyle(
-                          fontStyle: FontStyle.italic,
-                          color: Colors.lightBlueAccent),)
-                  ],
-                ),
-              ))
+                  ),
+                ],
+              ),
+            ),
+            decoration: new BoxDecoration(
+              color: Colors.blue
+            ),
+          ),
+          new ListTile(
+            leading: new Icon(Icons.photo_album),
+            title: new Text("Photo"),
+            onTap: (){
+              setState(() {
+                  this.title = 'This is Photos page';
+              });
+              Navigator.pop(context);
+            },
+          ),
+          new ListTile(
+            leading: new Icon(Icons.notifications),
+            title: new Text("Notifications"),
+            onTap: (){
+              setState(() {
+                  this.title = 'This is Notifications';
+              });
+              Navigator.pop(context);
+            },
+          ),
+          new ListTile(
+            leading: new Icon(Icons.settings),
+            title: new Text("Settings"),
+          ),
+          new Divider(
+            color: Colors.black45,
+            indent: 16.0,
+          ),
+          new ListTile(
+            title: new Text("About Me"),
+          ),
+          new ListTile(
+            title: new Text("Privacy"),
+          ),
         ],
       ),
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -59,13 +103,10 @@ class __StatefulWidgetState extends State<_StatefulWidget> {
       body: new Center(
         child: new Text(
           this.title,
-          style: new TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30.0),),
+          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
+        ),
       ),
-        drawer: drawer(context),
+      drawer: drawer(context),
     );
   }
 }
-
-
